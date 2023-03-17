90f8964da078a8adfe174fc64bdf76f40442b76e
kosovic-drasko <kosovic.drasko@montefarmco.me>
2023-03-17 09:05:59 +0100

    init

--- public/app.js
+++ public/app.js
@@ -1,3 +1,26 @@
 "use strict";
-const anchor = document.querySelector('a');
-console.log(anchor);
+// const anchor=document.querySelector('a')!;
+// console.log(anchor.href);
+// const form=document.querySelector('.new-item-form')
+// const type=document.querySelector('#type') as HTMLSelectElement;
+// const details=document.querySelector('#details') as HTMLInputElement;
+// const amount=document.querySelector('#amount') as HTMLInputElement;
+// form?.addEventListener('submit',(e:Event)=>{
+//     e.preventDefault();
+//     console.log(
+//         type.value,
+//         details.value,
+//         amount.valueAsNumber
+//     )
+// })
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c,
+            this.details = d,
+            this.amount = a;
+    }
+}
+const invOne = new Invoice('drasko', 'radi na odrzavanju', 400);
+console.log(invOne);
+invOne.client = "marko";
+console.log(invOne);
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,36 @@
-const anchor=document.querySelector('a');
-console.log(anchor);
+// const anchor=document.querySelector('a')!;
+// console.log(anchor.href);
+
+// const form=document.querySelector('.new-item-form')
+
+// const type=document.querySelector('#type') as HTMLSelectElement;
+// const details=document.querySelector('#details') as HTMLInputElement;
+// const amount=document.querySelector('#amount') as HTMLInputElement;
+// form?.addEventListener('submit',(e:Event)=>{
+//     e.preventDefault();
+//     console.log(
+//         type.value,
+//         details.value,
+//         amount.valueAsNumber
+//     )
+// })
+
+ class Invoice{
+client:string;
+details:string;
+amount:number
+ 
+constructor(c:string,d:string,a:number){
+    this.client=c,
+    this.details=d,
+    this.amount=a
+}
+// format(){
+//     return`$(this.client) posjeduje $(this.amunt) za $(this.details)`;
+// }
+ }
+const invOne=new Invoice('drasko','radi na odrzavanju',400);
+console.log(invOne);
+
+invOne.client="marko";
+console.log(invOne);
